821d3960868fca452cdf5432788ecc3e7cd4f3b5
Zachary <zacharychang.zc@gmail.com>
2015-12-19 19:49:18 +0800

    learn to use luasql to connect to mysql

--- controller/admin/new_tab.lua
+++ controller/admin/new_tab.lua
@@ -8,6 +8,7 @@ function index()
 	entry({"admin","new_tab","tab_from_view"},template("admin_myapp/view_tab"),"View Tab",2)
 	entry({"admin","new_tab","action_counter"},call("counter"),_("Click here"),3).leaf=true
 	entry({"admin","new_tab","reboot"},cbi("admin_myapp/reboot"),_("Reboot"),4)
+	entry({"admin","new_tab","get_user"},call("getuser"),_("Show User"),5)
 end
 
 function counter()
@@ -19,4 +20,21 @@ function counter()
 	fs.writefile("/var/run/test",string.format("%d\n",count))
 	luci.http.write(tostring(count))
 	return
+end
+
+function getuser()
+	sql=require "luasql.mysql"
+	local env = sql.mysql()
+	local conn = env:connect('test','root','5238222','127.0.0.1',3306)
+
+	cursor,errorInfo=conn:execute([[select * from user;]])
+
+	row=cursor:fetch({},"a")
+	while row do
+		luci.http.write(string.format("Id: %s, Name: %s",row.id,row.username))
+		row=cursor:fetch(row,"a")
+	end
+	cursor:close()
+	conn:close()
+	env:close()
 end
