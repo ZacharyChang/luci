c077c5b13239ba9f6fc22842dfe98acc179f6b5c
Zachary <zacharychang.zc@gmail.com>
2015-12-25 20:17:43 +0800

    delete unnecessary test parts

--- controller/admin/my_blog.lua
+++ controller/admin/my_blog.lua
@@ -3,11 +3,10 @@ module("luci.controller.admin.my_blog",package.seeall)
 function index()
 	entry({"admin","my_blog"},firstchild(),"Blog",30).dependent=false
 	entry({"admin","my_blog","publish_blog"},template("admin_myapp/add_blog"),_("Publish Blog"),1)
-	entry({"admin","my_blog","test_blog"},template("admin_myapp/test_blog"),_("Test"),2)
-	entry({"admin","my_blog","show_blog"},template("admin_myapp/list_blog"),_("Show Blog"),3)
-	entry({"admin","my_blog","view_blog"},call("blog_view"),_("View Blog"),4)
-	entry({"admin","my_blog","update_blog"},call("blog_update"),_("Update Blog"),5)
-	entry({"admin","my_blog","remove_blog"},call("blog_remove"),_("Remove Blog"),6)
+	entry({"admin","my_blog","show_blog"},template("admin_myapp/list_blog"),_("Show Blog"),2)
+	entry({"admin","my_blog","view_blog"},call("blog_view"),_("View Blog"),3)
+	entry({"admin","my_blog","update_blog"},call("blog_update"),_("Update Blog"),4)
+	entry({"admin","my_blog","remove_blog"},call("blog_remove"),_("Remove Blog"),5)
 end
 
 function blog_view()
